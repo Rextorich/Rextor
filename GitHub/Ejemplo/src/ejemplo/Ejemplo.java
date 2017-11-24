4f9c2aab57dbad0213e97dfbfe4272842a3ae91e
rexto <rexto@10.5.13.195>
2017-11-23 19:06:17 -0500

    Update EjercicioGit.java
    Escribi otro mensaje

--- GitHub/Ejemplo/src/ejemplo/Ejemplo.java
+++ GitHub/Ejemplo/src/ejemplo/Ejemplo.java
@@ -5,6 +5,8 @@
  */
 package ejemplo;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author rexto
@@ -16,6 +18,8 @@ public class Ejemplo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "Hoola");
     }
     
 }
